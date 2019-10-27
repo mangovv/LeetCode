7c18b96f3a9fbca6aa6b9a07094b797c8f0fec04
Trinity <ohmangogo@gmail.com>
2019-10-27 15:54:56 -0400

    LC16

--- 16. 3Sum Closest/16. 3Sum Closest.js
+++ 16. 3Sum Closest/16. 3Sum Closest.js
@@ -7,7 +7,7 @@ var threeSumClosest = function(nums, target) {
   nums.sort((a, b) => a - b);
   let ans = nums[0] + nums[1] + nums[nums.length - 1];
 
-  for (let i = 0; i < nums.length - 1; i++) {
+  for (let i = 0; i < nums.length - 2; i++) {
     let start = i + 1;
     let end = nums.length - 1;
 
